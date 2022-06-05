92be057c79d421e829bf3daf93e89e3c0f1f08d3
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-06-05 03:49:31 +0200

    wip

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -2,11 +2,11 @@ import { withSession } from '@/lib/session'
 import { NextApiRequest, NextApiResponse } from '@/types/next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method != 'POST') return res.status(400).send()
+	if (req.method != 'POST') return res.status(400).end()
 
 	req.session.destroy()
 
-	res.send()
+	res.end()
 }
 
 export default withSession(handler)
